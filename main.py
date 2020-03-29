8128f119cca190d4b0f0e02d354bbb8c8bd72e6e
eusojk <josue.kpodo@gmail.com>
2020-03-29 17:28:57 -0400

    uploaded corrected fortran api in regards to SLCL (-99) in TH.SOL

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from soilapis.calculator import SoilConnector
-path_name = '/home/eusoj/Downloads/gis-scripts/proj-lstfd20/layers/soilproperties/'
 
-soil_conn = SoilConnector(path_name)
 
-soil_taw = soil_conn.get_total_available_water(103.84, 15.76, 500, 3)
-print(soil_taw)
-soil_dssat = soil_conn.get_soil_property(103.84, 15.76, 500, 3, "dssat")
-print(soil_dssat)
+def main():
+    path_name = '/home/eusojk/Downloads/layers/soilproperties/'
+
+    soil_conn = SoilConnector(path_name)
+
+    soil_taw = soil_conn.get_total_available_water(103.84, 15.76, 500, 3)
+    print(soil_taw)
+    soil_dssat = soil_conn.get_soil_property(103.84, 15.76, 500, 3, "dssat")
+    print(soil_dssat)
+
+
+if __name__ == '__main__':
+    main()
