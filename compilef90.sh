64b1849660d36d3a95c11774dcafc6ae9ee0ece5
Josué K <60592551+eusojk@users.noreply.github.com>
2020-04-16 16:16:54 -0400

    Create compilef90.sh

---
+++ compilef90.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if ! [ -x "$(command -v gfortran)" ]; then
+  echo 'Error: gfortran is not installed. Please install before you proceed' >&2
+  exit 1
+fi
+
+rootdir=$PWD
+soilapidir="$rootdir/soilapis"
+dotsolexec="$soilapidir/dotSolAPI2.a"
+mktxtexec="$soilapidir/make_texture.a"
+dotsoldir="$rootdir/soilapis/f90/dotsol"
+texturdir="$rootdir/soilapis/f90/texture"
+
+## compile dotSolApi with gfortran and move to parent dir
+cd $dotsoldir
+make clean
+make all
+cp dotSolAPI2.a $dotsolexec
+
+cd $texturdir
+make clean
+make all
+cp make_texture.a $mktxtexec
+
+echo "done compiling the fortran code"
