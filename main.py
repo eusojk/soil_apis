19ab8dd45ed6117b1742fd08cbbe06fca2bf61d6
Josue K <josue.kpodo@gmail.com>
2020-06-03 19:10:53 -0400

    testing main.py

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from soilapis.calculator import SoilConnector
 
 
 def main():
-    path_name = '/home/eusojk/downloads/soil_apis/soilproperties/'
+    path_name = '/home/eusojk/downloads/soilproperties/'
     ouput_path = "/home/eusojk/downloads/"
     soil_conn = SoilConnector(path_name)
-    lon = 103.98
-    lat = 11.725833333333334
-    # soil_taw = soil_conn.get_total_available_water(103.84, 15.76, 500, 3)
-    # print(soil_taw)
-    soil_dssat = soil_conn.get_soil_property(lon, lat, 600, 20, "dssat")
-    print(soil_dssat)
+    lon = 102.730077 #103.98
+    lat = 16.687557 # 11.725833333333334
+    soil_taw = soil_conn.get_total_available_water(103.84, 15.76, 500, 3)
+    print(soil_taw)
 
+    # list_depths = [ x for x in range(100, 520, 20)]
+    # list_taw = []
+    # for depth in list_depths:
+    #     #soil_dssat = soil_conn.get_soil_property(lon, lat, depth, 3)
+    #     soil_taw = soil_conn.get_total_available_water(lon, lat, depth, 3)
+    #     print(soil_taw , depth)
+    #     list_taw.append(soil_taw)
+    # print(list_taw)
+    
     # all_tif = soil_conn.get_all_tif_files()
     # for fi in all_tif:
     #     fi = glob.glob(fi)
